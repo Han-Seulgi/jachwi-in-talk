6632ab4b60bc7bbb91288edb1c5a2af6366e4d32
jiweon21 <jna2158@naver.com>
2020-05-28 23:35:17 +0900

    지원_수정

--- app/src/main/java/com/example/project_test/MainActivity.java
+++ app/src/main/java/com/example/project_test/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.project_test;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("프로젝트 만들기");
+        TabHost tabHost = getTabHost();
+
+        TabSpec tabSpecTab1 = tabHost.newTabSpec("TAB1").setIndicator("게시판");
+        tabSpecTab1.setContent(R.id.tab1);
+        tabHost.addTab(tabSpecTab1);
+
+        TabSpec tabSpecTab2 = tabHost.newTabSpec("TAB2").setIndicator("방구하기");
+        tabSpecTab2.setContent(R.id.tab2);
+        tabHost.addTab(tabSpecTab2);
+
+        TabSpec tabSpecTab3 = tabHost.newTabSpec("TAB3").setIndicator("행사알림");
+        tabSpecTab3.setContent(R.id.tab3);
+        tabHost.addTab(tabSpecTab3);
+
+        TabSpec tabSpecTab4 = tabHost.newTabSpec("TAB4").setIndicator("안전알림");
+        tabSpecTab4.setContent(R.id.tab4);
+        tabHost.addTab(tabSpecTab4);
+
+        tabHost.setCurrentTab(0);
+
     }
 }
+
