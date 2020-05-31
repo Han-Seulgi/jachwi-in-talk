9feb343d4ba0464708aaba7f349a469aaf2d8798
jiweon21 <jna2158@naver.com>
2020-05-31 22:43:00 +0900

    지원_메인

--- app/src/main/java/com/example/project_test/MainActivity.java
+++ app/src/main/java/com/example/project_test/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.project_test;
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends TabActivity {
-
+    ViewFlipper vflip;
+    ImageButton btn1, btn2, btn3, btn4, btn5, btn6;
+    ImageButton imageButtons[] = {btn1, btn2, btn3, btn4, btn5, btn6};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        vflip = findViewById(R.id.vflip);
+        imageButtons[0] = findViewById(R.id.btn1);
+        imageButtons[1] = findViewById(R.id.btn2);
+        imageButtons[2] = findViewById(R.id.btn3);
+        imageButtons[3] = findViewById(R.id.btn4);
+        imageButtons[4] = findViewById(R.id.btn5);
+        imageButtons[5] = findViewById(R.id.btn6);
+
+        //탭 만들기
         TabHost tabHost = getTabHost();
 
         ImageView tab1 = new ImageView(this);
@@ -38,6 +53,58 @@ public class MainActivity extends TabActivity {
 
         tabHost.setCurrentTab(0);
 
+        //첫 번째 탭의 뷰 플리퍼
+        int images[] = {R.drawable.food, R.drawable.meet}; //뷰 플리퍼에 들어갈 이미지
+
+        for(int image :images) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(image);
+            vflip.addView(imageView);      // 이미지 추가
+            vflip.setFlipInterval(4000);   // 몇 초 후에 이미지가 넘어갈것인가(1000 당 1초)
+            vflip.setAutoStart(true);      //자동시작유무(true:자동)
+            vflip.setInAnimation(this,android.R.anim.slide_in_left); //animation
+            vflip.setOutAnimation(this,android.R.anim.slide_out_right); //animation
+        }
+
+        vflip.setOnClickListener(new View.OnClickListener() { //뷰 플리퍼 클릭했을 때
+         @Override
+            public void onClick(View v) {
+                //어떤 이미지 클릭했는지 확인하고 해당 게시물 화면으로 넘어가게
+            }
+        });
+
+        //첫 번째 탭 6개의 게시판
+        for(int i=0; i<imageButtons.length; i++) {
+            imageButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(i == 0) {
+                        //자취앤집밥 게시판으로 넘어가기
+                    }
+                    else if (i == 1) {
+                        //자취인디자인 게시판으로 넘어가기
+                    }
+                    else if (i == 2) {
+                        //자취인만남 게시판으로 넘어가기
+                    }
+                    else if (i == 3) {
+                        //자취Q&A 게시판으로 넘어가기
+                    }
+                    else if (i == 4) {
+                        //자취인정보 게시판으로 넘어가기
+                    }
+                    else if (i == 5) {
+                        //자취인혼밥 게시판으로 넘어가기
+                    }
+                    else {
+                        //예외처리
+                    }
+                }
+            });
+        }
     }
+
+
+
 }
 
