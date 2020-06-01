aaf76c371f38fbb88b402f9f368b8f4d8149efb7
jiweon21 <jna2158@naver.com>
2020-06-01 17:59:10 +0900

    지원_메인_틀 완성

--- app/src/main/java/com/example/project_test/MainActivity.java
+++ app/src/main/java/com/example/project_test/MainActivity.java
@@ -2,23 +2,32 @@ package com.example.project_test;
 import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.Toast;
+import android.widget.Toolbar;
 import android.widget.ViewFlipper;
 
-public class MainActivity extends TabActivity {
-    ViewFlipper vflip;
-    ImageButton btn1, btn2, btn3, btn4, btn5, btn6;
+public class MainActivity extends TabActivity{
+    ViewFlipper vflip1,vflip2;
+    ImageButton btn1, btn2, btn3, btn4, btn5, btn6, img1, img2;
     ImageButton imageButtons[] = {btn1, btn2, btn3, btn4, btn5, btn6};
+    Button changebtn;
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        vflip = findViewById(R.id.vflip);
+        vflip1 = findViewById(R.id.vflip1);
+        vflip2 = findViewById(R.id.vflip2);
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        changebtn = findViewById(R.id.changebtn);
+
         imageButtons[0] = findViewById(R.id.btn1);
         imageButtons[1] = findViewById(R.id.btn2);
         imageButtons[2] = findViewById(R.id.btn3);
@@ -43,7 +52,7 @@ public class MainActivity extends TabActivity {
         tabSpecTab2.setContent(R.id.tab2);
         tabHost.addTab(tabSpecTab2);
 
-        TabSpec tabSpecTab3 = tabHost.newTabSpec("TAB3").setIndicator("행사행사행사^^");
+        TabSpec tabSpecTab3 = tabHost.newTabSpec("TAB3").setIndicator("행사");
         tabSpecTab3.setContent(R.id.tab3);
         tabHost.addTab(tabSpecTab3);
 
@@ -53,20 +62,20 @@ public class MainActivity extends TabActivity {
 
         tabHost.setCurrentTab(0);
 
-        //첫 번째 탭의 뷰 플리퍼
-        int images[] = {R.drawable.food, R.drawable.meet}; //뷰 플리퍼에 들어갈 이미지
+        //첫 번째 탭의 게시 인기글 뷰 플리퍼
+        int f1_images[] = {R.drawable.food, R.drawable.meet}; //뷰 플리퍼에 들어갈 이미지
 
-        for(int image :images) {
+        for(int image :f1_images) {
             ImageView imageView = new ImageView(this);
             imageView.setBackgroundResource(image);
-            vflip.addView(imageView);      // 이미지 추가
-            vflip.setFlipInterval(4000);   // 몇 초 후에 이미지가 넘어갈것인가(1000 당 1초)
-            vflip.setAutoStart(true);      //자동시작유무(true:자동)
-            vflip.setInAnimation(this,android.R.anim.slide_in_left); //animation
-            vflip.setOutAnimation(this,android.R.anim.slide_out_right); //animation
+            vflip1.addView(imageView);      // 이미지 추가
+            vflip1.setFlipInterval(4000);   // 몇 초 후에 이미지가 넘어갈것인가(1000 당 1초니까 4초후에 넘어가는거야)
+            vflip1.setAutoStart(true);      //자동시작유무(true:자동)
+            vflip1.setInAnimation(this,android.R.anim.slide_in_left); //animation
+            vflip1.setOutAnimation(this,android.R.anim.slide_out_right); //animation
         }
 
-        vflip.setOnClickListener(new View.OnClickListener() { //뷰 플리퍼 클릭했을 때
+        vflip1.setOnClickListener(new View.OnClickListener() { //뷰 플리퍼 클릭했을 때
          @Override
             public void onClick(View v) {
                 //어떤 이미지 클릭했는지 확인하고 해당 게시물 화면으로 넘어가게
@@ -102,6 +111,51 @@ public class MainActivity extends TabActivity {
                 }
             });
         }
+
+        //첫 번째 탭의 방 구하기 게시물 뷰 플리퍼
+        int f2_images[] = {R.drawable.food, R.drawable.meet}; //뷰 플리퍼에 들어갈 이미지
+
+        for(int image :f2_images) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(image);
+            vflip2.addView(imageView);      // 이미지 추가
+            vflip2.setFlipInterval(4000);   // 몇 초 후에 이미지가 넘어갈것인가(1000 당 1초)
+            vflip2.setAutoStart(true);      //자동시작유무(true:자동)
+            vflip2.setInAnimation(this,android.R.anim.slide_in_left); //animation
+            vflip2.setOutAnimation(this,android.R.anim.slide_out_right); //animation
+        }
+
+        //첫 번째 탭의 방 구하기 게시물 뷰 플리퍼 클릭했을 때
+        vflip2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //어떤 이미지 클릭했는지 확인하고 해당 게시물 화면으로 넘어가게
+            }
+        });
+
+        //첫 번째 탭 나눔 HOT에서 다음 버튼을 눌렀을 때
+        changebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //i는 0으로 초기화되어있음
+                if ( i == 0 ){
+                    img1.setImageResource(R.drawable.meet);
+                    img2.setImageResource(R.drawable.fish);
+                    i--;
+                }
+                else if ( i == -1 ){
+                    img1.setImageResource(R.drawable.naverlogo2);
+                    img2.setImageResource(R.drawable.backbtn);
+                    i--;
+                }
+                else {
+                    img1.setImageResource(R.drawable.check);
+                    img2.setImageResource(R.drawable.backbtn);
+                    i=0;
+                }
+
+            }
+        });
     }
 
 
