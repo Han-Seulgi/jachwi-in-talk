89c5763c8129b18675eb758299b046d91ab7df55
Han-Seulgi <skhan20@naver.com>
2020-06-05 16:51:32 +0900

    슬기_안전지키미 틀

--- app/src/main/java/com/example/project_test/EmergencyActivity.java
+++ app/src/main/java/com/example/project_test/EmergencyActivity.java
@@ -1,24 +1,36 @@
 package com.example.project_test;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class EmergencyActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
 
+        GridView gv = findViewById(R.id.gv1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+
         //상단탭
         toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayShowCustomEnabled(true);
@@ -49,5 +61,41 @@ public class EmergencyActivity extends AppCompatActivity {
                 return true;
         }
         return true;
+    }
+}
+
+class MyGridAdapter extends BaseAdapter {
+    Context context;
+
+    public MyGridAdapter(Context _context) {
+        context = _context;
+    }
+
+    @Override
+    public int getCount() {
+        return iconID.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    Integer[] iconID = {R.drawable.meet, R.drawable.meet,R.drawable.meet,R.drawable.meet,R.drawable.meet,R.drawable.meet};
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageButton btnG = new ImageButton(context);
+        btnG.setLayoutParams(new GridView.LayoutParams(300,300));
+        btnG.setScaleType(ImageButton.ScaleType.FIT_CENTER);
+        btnG.setPadding(5,5,5,5);
+        btnG.setImageResource(iconID[position]);
+
+        return btnG;
     }
 }
