f7c70907972431abff25c4b903ff174d13e06722
Han-Seulgi <skhan20@naver.com>
2020-06-11 21:46:06 +0900

    슬기_안전관리틀&손전등&문자전송(실패)

--- app/src/main/java/com/example/project_test/EmergencyActivity.java
+++ app/src/main/java/com/example/project_test/EmergencyActivity.java
@@ -1,33 +1,63 @@
 package com.example.project_test;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+
 public class EmergencyActivity extends AppCompatActivity {
+    Activity act = this;
     Toolbar toolbar;
+    GridView gv;
+
+    ArrayList<Bitmap> picArr = new ArrayList<Bitmap>();//이미지배열
+    ArrayList<String> textArr = new ArrayList<String>();//텍스트배열열
+    Bitmap bm1, bm2, bm3, bm4, bm5, bm6;
+
+    private boolean mFlashOn;//손전등 켬/끔
+    private CameraManager cm;
+    private String mCameraId;
+
+    TelephonyManager tm;
+    String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
-
-        GridView gv = findViewById(R.id.gv1);
-        MyGridAdapter gAdapter = new MyGridAdapter(this);
-        gv.setAdapter(gAdapter);
 
         //상단탭
         toolbar = findViewById(R.id.toolbar);
@@ -37,6 +67,74 @@ public class EmergencyActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // ↓툴바의 홈버튼의 이미지를 변경(기본 이미지는 뒤로가기 화살표)
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.fish);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //그리드뷰
+        bm1 = BitmapFactory.decodeResource(getResources(), R.drawable.flash);
+        bm2 = BitmapFactory.decodeResource(getResources(), R.drawable.message);
+        bm3 = BitmapFactory.decodeResource(getResources(), R.drawable.shakemap);
+        bm4 = BitmapFactory.decodeResource(getResources(), R.drawable.siren);
+        bm5 = BitmapFactory.decodeResource(getResources(), R.drawable.setting);
+        bm6 = BitmapFactory.decodeResource(getResources(), R.drawable.call);
+        Bitmap bms[] = {bm1, bm2, bm3, bm4, bm5, bm6};
+        String str[] = {"손전등", "위치전송", "흔들림횟수", "경고음", "설정", "긴급전화"};
+        for (int i = 0; i < bms.length; i++) {
+            picArr.add(bms[i]);
+        }
+        for (int i = 0; i < str.length; i++) {
+            textArr.add(str[i]);
+        }
+        gv = findViewById(R.id.gv1);
+        MyGridAdapter gAdapter = new MyGridAdapter();
+        gv.setAdapter(gAdapter);
+
+        //카메라
+        cm = (CameraManager) getSystemService(CAMERA_SERVICE);
+        //핸드폰 정보
+        tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS}, MODE_PRIVATE);
+        } else {
+            phoneNumber = tm.getLine1Number();}
+
+
+
+
+        //이벤트
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //@RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+                            Toast.makeText(getApplicationContext(), "핸드폰이 손전등기능을 지원하지 않아\n사용하실 수 없습니다", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        flashlight();
+                        return;
+
+                    case 1:
+                        Toast.makeText(getApplicationContext(), "전송완료", Toast.LENGTH_SHORT).show();
+                        try {
+
+                                String sms = "테스트입니당";
+                                SmsManager smsManager = SmsManager.getDefault();
+                                smsManager.sendTextMessage(phoneNumber, null, sms, null, null);
+                                Toast.makeText(getApplicationContext(), "전송완료", Toast.LENGTH_SHORT).show();
+
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "전송실패", Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }return;
+
+                    case 2: return;
+                    case 3: return;
+                    case 4: return;
+                    case 5: return;
+
+                }//switch end
+            }
+        });
     }
 
     //상단탭 메뉴
@@ -62,40 +160,75 @@ public class EmergencyActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    //어댑터
+    public class MyGridAdapter extends BaseAdapter {
+        LayoutInflater inflater;
+
+        public MyGridAdapter() {
+            inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        //그리드뷰에 출력할 목록 수
+        @Override
+        public int getCount() {
+            return picArr.size();
+        }
+
+        //아이템 호츌
+        @Override
+        public Object getItem(int position) {
+            return picArr.get(position);
+        }
+
+        //아이템ID
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.emergency_list, parent, false);
+            }
+            final ImageView imageView = (ImageView) convertView.findViewById(R.id.imgView1);
+            TextView textView = (TextView) convertView.findViewById(R.id.tv1);
+            imageView.setImageBitmap(picArr.get(position));
+            textView.setText(textArr.get(position));
+
+
+            return convertView;
+        }
+    }
+
+    //손전등 켜기
+    //@RequiresApi(api = Build.VERSION_CODES.M)
+    void flashlight() {
+        if (mCameraId == null) {
+            try {
+                for (String id : cm.getCameraIdList()) {
+                    CameraCharacteristics c = cm.getCameraCharacteristics(id);
+                    Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                    Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                    if (flashAvailable != null && flashAvailable && lensFacing != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                        mCameraId = id;
+                        break;
+                    }
+                }
+            } catch (CameraAccessException e) {
+                mCameraId = null;
+                e.printStackTrace();
+                return;
+            }
+        }
+        mFlashOn = !mFlashOn;
+        try {
+            cm.setTorchMode(mCameraId, mFlashOn);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
-class MyGridAdapter extends BaseAdapter {
-    Context context;
 
-    public MyGridAdapter(Context _context) {
-        context = _context;
-    }
-
-    @Override
-    public int getCount() {
-        return iconID.length;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    Integer[] iconID = {R.drawable.meet, R.drawable.meet,R.drawable.meet,R.drawable.meet,R.drawable.meet,R.drawable.meet};
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageButton btnG = new ImageButton(context);
-        btnG.setLayoutParams(new GridView.LayoutParams(300,300));
-        btnG.setScaleType(ImageButton.ScaleType.FIT_CENTER);
-        btnG.setPadding(5,5,5,5);
-        btnG.setImageResource(iconID[position]);
-
-        return btnG;
-    }
-}
